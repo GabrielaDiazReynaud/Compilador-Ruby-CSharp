58f004e5379cae79ae84893484632bd742e8e560
Gabriela Paola Diaz Reynaud <122854405+GabrielaDiazReynaud@users.noreply.github.com>
2023-02-11 00:54:04 -0600

    Add files via upload

---
+++ Compilador_Clase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using MiniLanguageCompiler.Infrastructure;
+using MiniLanguageCompiler.Lexer;
+
+namespace MiniLanguageCompiler.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var fileContent = File.ReadAllText("test.txt");
+            var logger = new Logger();
+            var scanner = new Scanner(new Input(fileContent), logger);
+            var token = scanner.GetNextToken();
+            while (token.TokenType != Core.TokenType.EOF)
+            {
+                logger.Info(token.ToString());
+                token = scanner.GetNextToken();
+            }
+        }
+    }
+}
---
+++ MiniLanguageCompiler.Core/TokenType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MiniLanguageCompiler.Core
+{
+    public enum TokenType
+    {
+       Plus,
+       Minus,
+       Equal,
+       Colon,
+       Semicolon,
+       LogicalAnd,
+       LogicalOr,
+       LeftParens,
+       RightParens,
+       Multiplication,
+       Division,
+       GreaterThan,
+       LessThan,
+       GreaterOrEqualThan,
+       LessOrEqualThan,
+       LeftBracket,
+       RightBracket,
+       LeftBrace,
+       RightBrace,
+       Comma,
+       IfKeyword,
+       WhileKeyword,
+       ElseKeyword,
+       NumberKeyword,
+       Identifier,
+       StringKeyword,
+       EOF,
+       Assignation,
+       StringLiteral,
+       NumberLiteral,
+       IntArrayKeyword,
+       StringArrayKeyword,
+    }
+}
